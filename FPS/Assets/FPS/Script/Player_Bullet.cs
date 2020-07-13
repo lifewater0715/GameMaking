8f99042d2ad6e68b260f192fa41e05329d440786
Lifewater0715 <dydte1206@naver.com>
2020-07-13 19:17:21 +0900

    UniRun start

---
+++ FPS/Assets/FPS/Script/Player_Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bullet : MonoBehaviour
+{
+    public float B_Speed;
+    public Vector3 s_speed;
+    public float Timer=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if(Timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        //rigidbody.velocity = s_speed * B_Speed;
+
+        transform.position += B_Speed * s_speed * Time.deltaTime;
+    }
+}
---
+++ FPS/Assets/FPS/Script/Player_Move.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float P_Speed = 1;
+    public float ATS = 1;
+    public float Timer = 1;
+
+    public float Rebound = 10;
+
+    public GameObject Bullet;
+    public Transform A;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = ATS;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        float r = Random.Range(Rebound, -Rebound);
+        Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            Timer = ATS;
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("발사");
+                GameObject bullet = GameObject.Instantiate(Bullet);
+                bullet.transform.position = transform.position;
+                bullet.transform.right = transform.up + new Vector3(0, r, 0);
+                var b = bullet.GetComponent<Player_Bullet>();
+                b.s_speed = transform.right + new Vector3(r, 0 , 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * P_Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * P_Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * P_Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * P_Speed * Time.deltaTime;
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
