d19c5bb8e1eba59c2e62db23eccc51ac9c0b5306
Lifewater0715 <dydte1206@naver.com>
2020-07-21 21:24:07 +0900

    1

--- ShootingGame/Assets/ShootingGame/Script/PlayerControl.cs
+++ ShootingGame/Assets/ShootingGame/Script/PlayerControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float PlayerSpeed;
+    public float FierTimer;
+    public float fierTimer;
+
     public Transform m_MisPoint;
 
     public GameObject Mis;
@@ -17,26 +20,31 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        var X = gameObject.transform.position.x;
+        var Y = gameObject.transform.position.y;
+        //--Move
+        if (Input.GetKey(KeyCode.W) && Y <= 5.5)
         {
             transform.position += Vector3.up * PlayerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && Y >= -5.5)
         {
             transform.position += Vector3.down * PlayerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && X >= -10.5)
         {
             transform.position += Vector3.left * PlayerSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && X <= 10.5)
         {
             transform.position += Vector3.right * PlayerSpeed * Time.deltaTime;
         }
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        //--Shoot
+        FierTimer -= Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && FierTimer <= 0)
         {
             Shoot();
+            FierTimer = fierTimer;
         }
     }
 
