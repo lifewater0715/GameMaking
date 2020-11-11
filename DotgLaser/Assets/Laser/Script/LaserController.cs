8892e02639bc2fcd8143ae2d9529e97056de97a5
Lifewater0715 <dydte1206@naver.com>
2020-11-11 18:41:45 +0900

    레이저 피하기 vor 1.0.0

---
+++ DotgLaser/Assets/Laser/Script/LaserController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public GameObject Laser1;
+    public GameObject Laser1Instantiate;
+    public bool updown;
+
+    public float Timer1;
+    private float timer1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer1 = Timer1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer1 -= Time.smoothDeltaTime;
+        if(Timer1 <= 0)
+        {
+            Timer1 = timer1;
+            
+            var Randupdown = Random.Range(-1,2);
+            if (Randupdown == 0) updown = true;
+            else updown = false;
+            var XRandpont = Random.Range(8f,-8f);
+            LasreType1(updown, XRandpont);
+        }
+    }
+    public void LasreType1(bool updown, float Xpoint)
+    {
+        Laser1Instantiate = GameObject.Instantiate(Laser1);
+        if(updown==true)
+        {
+            Laser1Instantiate.transform.localPosition = new Vector2(Xpoint, 5);
+        }
+        else
+        {
+            Laser1Instantiate.transform.localPosition = new Vector2(Xpoint, -5);
+        }
+        
+    }
+}
---
+++ DotgLaser/Assets/Laser/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float PlayerSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) //좌
+        {
+            transform.position += Vector3.left * PlayerSpeed * Time.smoothDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) //우
+        {
+            transform.position += Vector3.right * PlayerSpeed * Time.smoothDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.W)) //상
+        {
+            transform.position += Vector3.up * PlayerSpeed * Time.smoothDeltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) //하
+        {
+            transform.position += Vector3.down * PlayerSpeed * Time.smoothDeltaTime;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Laser")
+        {
+            Debug.Log("Player Die");
+            Destroy(gameObject);
+        }
+    }
+}
+
